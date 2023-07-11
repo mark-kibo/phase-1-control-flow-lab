ed205dbcb0cff0efe4b0498fd3807c5086a51d94
mark-kibo <kibochamark@gmail.com>
2023-07-11 14:21:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if(someValue <= 400){
+    return "This one is on me!";
+  }else if(someValue > 2000 && someValue <= 2500){
+    return "I will gladly take your thirty bucks.";
+  }else if(someValue > 2500){
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+  if(cityName == "NYC"){
+    return "Ok, sounds good.";
+  }else{
+    return "No go."
+  }
+
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(courtoesName){
   // Write your code here!
+  if(courtoesName === "generous"){
+    return "Thank you so much.";
+  }else if(courtoesName === "not as generous"){
+    return "Thank you.";
+  }else{
+    return "Bye.";
+  }
 }
+
